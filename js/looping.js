72419178d8e75eca92948c1667103f5eff326d73
Enzo <enoneon10@gmail.com>
2026-08-16 22:53:26 +0700

    maintenance gambar

--- js/looping.js
+++ js/looping.js
@@ -1,25 +1,42 @@
-var img = [0,1,2,3, 4, 5,6,7,8]
-let isinya = ""
-img.forEach(isi)
+var img = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-    function isi(g){
-        document.getElementById("isinya").innerHTML = isinya
-        isinya += ('<img id=gambar'+(g+1)+ 'geser src=looping_img/slider' +(g+1)+ '.jpg>') 
-    }
+let isinya = "";
 
-var img1 = [0,1,2,3,4,5,6,7,8 ]
-let isinya1 =""
-img.forEach(isi1)
+img.forEach(isi);
 
-     function isi1(g){
-    document.getElementById("isinya1").innerHTML = isinya1
-    isinya1 += ('<img id=gambar' +(g+1)+ 'geser src=looping_img/slider' +(g+1)+ '.jpg>')
+function isi(g) {
+
+    isinya += '<img id="gambar' + (g + 1) + '" class="geser" src="../img/slider' + (g + 1) + '.jpg" alt="Slider ' + (g + 1) + '">';
+
+    document.getElementById("isinya").innerHTML = isinya;
 }
 
-document.getElementById("logout-btn").onclick = function(){
+
+var img1 = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+let isinya1 = "";
+
+img1.forEach(isi1);
+
+function isi1(g) {
+
+    isinya1 += '<img id="gambar' + (g + 1) + '" class="geser" src="../img/slider' + (g + 1) + '.jpg" alt="Slider ' + (g + 1) + '">';
+
+    document.getElementById("isinya1").innerHTML = isinya1;
+}
+
+
+// ================= LOGOUT =================
+
+document.getElementById("logout-btn").onclick = function () {
+
     document.getElementById("logout-konten").style.scale = 1;
-    document.getElementById("logout-konten")
-}
-document.getElementById("no-btn").onclick = function(){
-    document.getElementById("logout-konten").style.scale = 0
-}
+
+};
+
+
+document.getElementById("no-btn").onclick = function () {
+
+    document.getElementById("logout-konten").style.scale = 0;
+
+};
