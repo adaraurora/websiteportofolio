5b0b167d97223d06858bbeeba1bed8427ed77ff2
Enzo <enoneon10@gmail.com>
2026-08-16 23:00:32 +0700

    maintenance gambar

--- js/looping.js
+++ js/looping.js
@@ -1,36 +1,65 @@
+// ================= SLIDER =================
+
 var img = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 let isinya = "";
 
-img.forEach(isi);
+img.forEach(function (g) {
 
-function isi(g) {
-    isinya += '<img id="gambar' + (g + 1) + '" class="geser" src="img/slider' + (g + 1) + '.jpg" alt="Slider ' + (g + 1) + '">';
-    
-    document.getElementById("isinya").innerHTML = isinya;
+    isinya +=
+        '<img id="gambar' + (g + 1) +
+        '" class="geser" src="img/slider' + (g + 1) +
+        '.jpg" alt="Slider ' + (g + 1) + '">';
+
+});
+
+const slider1 = document.getElementById("isinya");
+
+if (slider1) {
+    slider1.innerHTML = isinya;
 }
 
+
+// ================= SLIDER 2 =================
 
 var img1 = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 let isinya1 = "";
 
-img1.forEach(isi1);
+img1.forEach(function (g) {
 
-function isi1(g) {
-    isinya1 += '<img id="gambar' + (g + 1) + '" class="geser" src="img/slider' + (g + 1) + '.jpg" alt="Slider ' + (g + 1) + '">';
-    
-    document.getElementById("isinya1").innerHTML = isinya1;
+    isinya1 +=
+        '<img id="gambar1-' + (g + 1) +
+        '" class="geser" src="img/slider' + (g + 1) +
+        '.jpg" alt="Slider ' + (g + 1) + '">';
+
+});
+
+const slider2 = document.getElementById("isinya1");
+
+if (slider2) {
+    slider2.innerHTML = isinya1;
 }
 
 
 // ================= LOGOUT =================
 
-document.getElementById("logout-btn").onclick = function () {
-    document.getElementById("logout-konten").style.scale = 1;
-};
+const logoutBtn = document.getElementById("logout-btn");
+const logoutKonten = document.getElementById("logout-konten");
+const noBtn = document.getElementById("no-btn");
 
+if (logoutBtn && logoutKonten) {
 
-document.getElementById("no-btn").onclick = function () {
-    document.getElementById("logout-konten").style.scale = 0;
-};
+    logoutBtn.onclick = function () {
+        logoutKonten.style.scale = 1;
+    };
+
+}
+
+if (noBtn && logoutKonten) {
+
+    noBtn.onclick = function () {
+        logoutKonten.style.scale = 0;
+    };
+
+}
